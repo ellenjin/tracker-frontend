af3021ddcda6e963909eb88d45262ca65c652cb5
Laura Castro <lauracastrotech@gmail.com>
2025-08-06 15:49:16 -0400

    removes test

---
+++ app/src/__tests__/NewGroupForm.test.jsx
@@ -0,0 +1,4 @@
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import GroupDetails from '../features/groups/GroupDetails';
+import { afterEach } from 'vitest';
