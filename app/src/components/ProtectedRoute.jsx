a2dd403353c65bd161b6feeef59c2f90e8c1c9df
Laura Castro <lauracastrotech@gmail.com>
2025-08-12 17:32:55 -0400

    removes unused imports

--- app/src/components/ProtectedRoute.jsx
+++ app/src/components/ProtectedRoute.jsx
@@ -1,9 +1,3 @@
-/*
-Determines whether or not someone is logged in or not -- if yes logged in,
-then will route to the assigned page. Else, the default route will be to redirect
-to the login page.
-*/
-
 import { Navigate } from 'react-router-dom';
 import NavHeader from './NavHeader';
 
