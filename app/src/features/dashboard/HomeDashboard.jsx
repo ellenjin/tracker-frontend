051b67a04c35810a60ccedd2c2708947de0d383d
Laura Castro <lauracastrotech@gmail.com>
2025-08-11 23:09:51 -0400

    fixes home dashboard

--- app/src/features/dashboard/HomeDashboard.jsx
+++ app/src/features/dashboard/HomeDashboard.jsx
@@ -1,27 +1,3 @@
-// import { Button, Box } from '@mui/material';
-
-// function NewLogBtn({
-//   isVisible,
-//   onClick,
-//   showText = 'Create a New Log!',
-//   hideText = 'Hide New Log Form',
-// }) {
-//   return (
-//     <Box sx={{ mt: 2, mb: 2 }}>
-//       <Button
-//         variant={isVisible ? 'outlined' : 'contained'}
-//         color="primary"
-//         onClick={onClick}
-//         sx={{ width: { xs: '100%', sm: 'auto' } }}
-//       >
-//         {isVisible ? hideText : showText}
-//       </Button>
-//     </Box>
-//   );
-// }
-
-// export default NewLogBtn;
-
 import React, { useState } from 'react';
 import NewLogForm from '../../features/logging/NewLogForm';
 import { Box, Typography } from '@mui/material';
