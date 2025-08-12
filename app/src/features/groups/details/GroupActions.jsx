57ddd1eebdca94d72d8da4d470e232ed239a31de
Solhee <ellen.solhee.jin@gmail.com>
2025-08-11 20:06:36 -0700

    Adjusted checkin message on group details page

--- app/src/features/groups/details/GroupActions.jsx
+++ app/src/features/groups/details/GroupActions.jsx
@@ -3,7 +3,7 @@ import MatchDropdown from './MatchDropDown';
 const GroupActions = ({ onRemind, groupUsers, groupId, handleCheckIn }) => {
   return (
     <>
-      <p>You haven't checked-in today!</p>
+      <p>Have you checked in today?</p>
       <button onClick={handleCheckIn}>Check-in</button>
       <button onClick={onRemind}>Remind Friends</button>
       <MatchDropdown groupUsers={groupUsers} groupId={groupId} />
