f39cd8cd6cd8193127780840fb42a0bca51aeed5
Laura Castro <lauracastrotech@gmail.com>
2025-08-12 01:30:02 -0400

    adds logs mui styles

---
+++ app/src/features/logging/SkeletonLogList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Skeleton,
+  ListItemText,
+} from '@mui/material';
+
+export default function SkeletonList() {
+  return (
+    <List>
+      {[...Array(5)].map((_, index) => (
+        <ListItem key={index} sx={{ px: 2, py: 1.5 }}>
+          <ListItemAvatar>
+            <Skeleton variant="circular" width={40} height={40} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={<Skeleton width="60%" />}
+            secondary={<Skeleton width="40%" />}
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+}
